d4f8592ffc0b71d17ef845fb49752a096809b972
HizoChen <chen166@shernet.sheridancollege.ca>
2024-03-14 17:49:27 -0400

    task4b done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public TextMeshProUGUI currentlytype;
+    void Start()
+    {
+        currentlytype.text = "Type of villager";
+    }
+
+    private void Update()
+    {
+        if (SelectedVillager != null)
+        {
+            currentlytype.text = SelectedVillager.GetType().ToString();
+        }
+
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
-        if(SelectedVillager != null)
+        if (SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
 }
