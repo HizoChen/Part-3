2ce6eb954bb900fa560dfed35456d6a708ea901e
HizoChen <chen166@shernet.sheridancollege.ca>
2024-03-21 11:28:51 -0400

    week10 task2done

---
+++ Assets/Week 10/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public Slider slider;
+    float time;
+    public float speed;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * speed;
+        time = time % 60;
+        slider.value = time;
+    }
+}
