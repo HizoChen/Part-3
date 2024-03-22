cd9c268f46142cd1c7c084d115e69b9d3b896f80
HizoChen <chen166@shernet.sheridancollege.ca>
2024-03-21 20:25:48 -0400

    week10-task4a done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class CharacterControl : MonoBehaviour
 {
+    public Villager[] array = new Villager[3];
+    public TMP_Dropdown dropdown;
     public TMPro.TextMeshProUGUI currentSelection;
     public static CharacterControl Instance;
+    private void Start()
+    {
+        Instance = this;
+    }
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -18,7 +25,11 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager.Selected(true);
         Instance.currentSelection.text = villager.ToString();
     }
+    public void DropdownHasChangedValue(int value)
 
+    {
+        SetSelectedVillager(array[value]);
+    }
     //private void Update()
     //{
     //    if(SelectedVillager != null)
